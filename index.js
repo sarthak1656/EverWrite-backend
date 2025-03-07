4505b70f99daf827749bea96420d602db96a9eb0
Sarthak Panigrahi <159803035+sarthak1656@users.noreply.github.com>
2025-03-07 23:01:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config({
   })
   const app = express();
   app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ever-write-frontend.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
